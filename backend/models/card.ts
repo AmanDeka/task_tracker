bc00e863b519eb417e9bc2a3ea37011d26a27ad6
Aman jyoti Deka <amanjyoti.deka2020@vitstudent.ac.in>
2024-02-04 21:55:11 +0530

    added card model

--- backend/models/card.ts
+++ backend/models/card.ts
@@ -13,10 +13,27 @@ mongoose.connect(process.env.MONGODB_URL||'');
 const cardModel = mongoose.model('Card',CardSchema);
 
 export const getCardByUserId = async (userId:string) => {
-    const card = await cardModel.find({userId:userId})
-    .select({_id:1,title:1,showDoneTasks:1})
-    .exec();
-    return card;
+    try{
+        const card = await cardModel.find({userId:userId})
+        .select({_id:1,title:1,showDoneTasks:1})
+        .exec();
+        return card;
+    }
+    catch(e){
+        throw e;
+    }
+}
+
+export const getCardById = async (Id:string) => {
+    try{
+        const card = await cardModel.findOne({_id:Id})
+        .select({_id:1,title:1,showDoneTasks:1})
+        .exec();
+        return card;
+    }
+    catch(e){
+        throw e;
+    }
 }
 
 
--- backend/routes/cardroutes.ts
+++ backend/routes/cardroutes.ts
@@ -1,5 +1,5 @@
 import express,{Request,Response}  from "express";
-import { getCardByUserId } from "../models/card";
+import { getCardByUserId ,getCardById} from "../models/card";
 
 const cardRoutes = express.Router();
 
@@ -17,7 +17,34 @@ cardRoutes.get('/all', async (req: Request, res: Response) => {
         const userCards = await getCardByUserId(userId);
 
 
-        res.status(200).json({ success: true, pages: userCards });
+        res.status(200).json({ success: true, cards: userCards });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ success: false, error: 'Internal Server Error' });
+    }
+});
+
+
+
+cardRoutes.get('/id/:cardId', async (req: Request, res: Response) => {
+    try {
+        let userId: string | null = null;
+        if (req.user) {
+            if ('id' in req.user && typeof req.user.id == "string") userId = req.user.id;
+        }
+
+        if (!userId) {
+            return res.status(400).json({ success: false, error: 'Not Authenticated' });
+        }
+
+        const card = await getCardById(req.params.cardId);
+
+        if(card?.userId !== userId){
+            return res.status(400).json({ success: false, error: 'Not Authenticated for current user' });
+        }
+
+
+        res.status(200).json({ success: true, card: card });
     } catch (error) {
         console.error(error);
         res.status(500).json({ success: false, error: 'Internal Server Error' });
